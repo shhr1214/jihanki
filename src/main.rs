04ff42f761e9aacb256b37d3f311c762eae1722b
shhr1214 <souhei0079@gmail.com>
2019-09-01 03:40:14 +0900

    テスト書いてみた

--- src/main.rs
+++ src/main.rs
@@ -16,3 +16,39 @@ struct Drink {
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn vending_machine_initialize_test() {
+        let vending_machine = VendingMachineBuilder::new()
+            .add(Drink::new("コーラ".into()), 120, 10)
+            .add(Drink::new("カルピス".into()), 120, 10)
+            .add(Drink::new("コーヒー".into()), 120, 10)
+            .build();
+
+        assert_eq!(
+            vending_machine,
+            VendingMachine {
+                stocks: vec![Stock {
+                    drink_name: "コーラ".into(),
+                    price: 120,
+                    drinks: vec![Drink::new("コーラ")],
+                    num: 10,
+                }, Stock {
+                    drink_name: "カルピス".into(),
+                    price: 120,
+                    drinks: vec![Drink::new("カルピス")],
+                    num: 10,
+                }, Stock {
+                    drink_name: "コーヒー".into(),
+                    price: 120,
+                    drinks: vec![Drink::new("コーヒー")],
+                    num: 10,
+                }].
+            }
+        );
+    }
+}
