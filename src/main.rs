f220cad482704ab0816aec2edb50e673beb7a7e3
shhr1214 <souhei0079@gmail.com>
2019-09-01 03:28:38 +0900

    今後使うであろう型
    使わないかもしれないやろう。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+struct VendingMachine {
+    stocks: Vec<Stock>,
+}
+
+struct Stock {
+    drink_name: String,
+    price: usize,
+    drinks: Vec<Drink>,
+    num: usize,
+}
+
+struct Drink {
+    name: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
